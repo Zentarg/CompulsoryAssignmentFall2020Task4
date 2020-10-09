621f2a8c9fa3b0e07aa1eb3ab229e5f51c7b7dc2
zentarg <apg.chili@gmail.com>
2020-10-09 09:46:46 +0200

    Initial commit

---
+++ CompulsoryAssignmentFall2020Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CompulsoryAssignmentFall2020Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
